e20c47496560394a5fc54fdacab43b1450be78ec
Adrien Roches <adrien@roc-it.tech>
2024-07-09 10:23:21 +0200

    [START] Use request in controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main_index')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return new Response('Hello World!');
+        $name = $request->query->get('name', 'World');
+
+        return new Response("Hello {$name}!");
     }
 }
