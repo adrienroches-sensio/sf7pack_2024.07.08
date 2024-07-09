16521c39ac424392437e7a418c06cea694299db9
Adrien Roches <adrien@roc-it.tech>
2024-07-09 14:41:48 +0200

    [START] Update EventController to persist new event entity

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -5,14 +5,22 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Event;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class EventController extends AbstractController
 {
-    #[Route('/event/{name}/{start}/{end}', name: 'app_event_new')]
-    public function newEvent(string $name, string $start, string $end, /* ... */): Response
+    #[Route('/event/{name}/{start}/{end}',
+        name: 'app_event_new',
+        requirements: [
+            'name' => '(\w|[- ])+',
+            'start' => '\d{2}-\d{2}-\d{4}',
+            'end' => '\d{2}-\d{2}-\d{4}',
+        ],
+    )]
+    public function newEvent(string $name, string $start, string $end, EntityManagerInterface $em): Response
     {
         $event = (new Event())
             ->setName($name)
@@ -22,7 +30,8 @@ class EventController extends AbstractController
             ->setEndAt(new \DateTimeImmutable($end))
         ;
 
-        //...
+        $em->persist($event);
+        $em->flush();
 
         return new Response('Event created');
     }
