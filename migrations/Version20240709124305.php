32947384b11b831f932d2f922d60a1aa24c2d1c4
Adrien Roches <adrien@roc-it.tech>
2024-07-09 17:10:35 +0200

    [START] FIX : missing description on latest migration

--- migrations/Version20240709124305.php
+++ migrations/Version20240709124305.php
@@ -14,7 +14,7 @@ final class Version20240709124305 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return '[Event] Make project optional.';
     }
 
     public function up(Schema $schema): void
