8a9ffee886608bc80b0bb016641a38c162a6a2c0
Adrien Roches <adrien@roc-it.tech>
2024-07-09 10:17:26 +0200

    [START] First controller with route

---
+++ src/Controller/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_main_index')]
+    public function index(): Response
+    {
+        return new Response('Hello World!');
+    }
+}
