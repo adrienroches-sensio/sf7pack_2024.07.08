bf74e6a6458cea6bbfeffc210c669f21c28c24d4
Adrien Roches <adrien@roc-it.tech>
2024-07-11 11:58:25 +0200

    [MASTER] Inject HttpClient instead of creating it

--- src/Search/Event/ApiEventSearch.php
+++ src/Search/Event/ApiEventSearch.php
@@ -4,20 +4,19 @@ declare(strict_types=1);
 
 namespace App\Search\Event;
 
-use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class ApiEventSearch implements EventSearchInterface
 {
     public function __construct(
+        private readonly HttpClientInterface $client,
         private readonly string $deveventsApiKey,
     ) {
     }
 
     public function searchByName(?string $name = null): array
     {
-        $client = HttpClient::create();
-
-        return $client->request('GET', 'https://www.devevents-api.fr/events', [
+        return $this->client->request('GET', 'https://www.devevents-api.fr/events', [
             'query' => [
                 'name' => $name ?? '',
             ],
