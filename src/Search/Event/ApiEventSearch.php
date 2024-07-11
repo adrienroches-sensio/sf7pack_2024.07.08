39121b26e06098ca7bb750c1b36ae5036451a0db
Adrien Roches <adrien@roc-it.tech>
2024-07-11 10:58:55 +0200

    [MASTER] Create base for ApiEventSearch class

---
+++ src/Search/Event/ApiEventSearch.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Search\Event;
+
+final class ApiEventSearch implements EventSearchInterface
+{
+    public function searchByName(?string $name = null): array
+    {
+        // TODO: Implement searchByName() method.
+    }
+}
