4feb7b80e2f9528347e656d430e56a21a734deed
Adrien Roches <adrien@roc-it.tech>
2024-07-11 11:42:53 +0200

    [MASTER] Inject api key using env variable

--- src/Search/Event/ApiEventSearch.php
+++ src/Search/Event/ApiEventSearch.php
@@ -6,6 +6,11 @@ namespace App\Search\Event;
 
 final class ApiEventSearch implements EventSearchInterface
 {
+    public function __construct(
+        private readonly string $deveventsApiKey,
+    ) {
+    }
+
     public function searchByName(?string $name = null): array
     {
         // TODO: Implement searchByName() method.
