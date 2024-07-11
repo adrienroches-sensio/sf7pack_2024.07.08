40cc76678a95e2add3942d4321a510d448abb86e
Adrien Roches <adrien@roc-it.tech>
2024-07-11 10:11:24 +0200

    [MASTER] Inject EventRepository in DatabaseSearchEvent using services.yaml

--- src/Search/Event/DatabaseEventSearch.php
+++ src/Search/Event/DatabaseEventSearch.php
@@ -5,14 +5,24 @@ declare(strict_types=1);
 namespace App\Search\Event;
 
 use App\Entity\Event;
+use App\Repository\EventRepository;
 
 final class DatabaseEventSearch
 {
+    public function __construct(
+        private readonly EventRepository $eventRepository,
+    ) {
+    }
+
     /**
      * @return list<Event>
      */
     public function searchByName(string|null $name = null): array
     {
-        return [];
+        if (null === $name) {
+            return $this->eventRepository->listAll();
+        }
+
+        return $this->eventRepository->searchByName($name);
     }
 }
