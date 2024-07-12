0d0a1fe3ce3db132cb2ebc5e157ec7c3b3bc7f85
Adrien Roches <adrien@roc-it.tech>
2024-07-12 09:57:14 +0200

    [MASTER] Secure project & event creation

--- src/Event/RemoteSearchEventListener.php
+++ src/Event/RemoteSearchEventListener.php
@@ -5,12 +5,26 @@ declare(strict_types=1);
 namespace App\Event;
 
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 #[AsEventListener]
 final class RemoteSearchEventListener
 {
+    public function __construct(
+        private readonly AuthorizationCheckerInterface $authorizationChecker,
+    ) {
+    }
+
     public function __invoke(RemoteSearchEvent $event): void
     {
+        if (
+            !$this->authorizationChecker->isGranted('ROLE_ORGANIZER')
+            && !$this->authorizationChecker->isGranted('ROLE_WEBSITE')
+        ) {
+            return;
+        }
+
+        // User is either ROLE_ORGANIZER or ROLE_WEBSITE
         dump($event);
     }
 }
