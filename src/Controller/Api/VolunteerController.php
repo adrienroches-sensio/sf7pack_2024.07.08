45593839ca8e0bcc37858268b36565627d0886d3
Adrien Roches <adrien@roc-it.tech>
2024-07-12 14:05:37 +0200

    [MASTER] Create new Controller class for Volunteer's API's

---
+++ src/Controller/Api/VolunteerController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use App\Repository\VolunteerRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+
+class VolunteerController extends AbstractController
+{
+    #[Route('/api/volunteer', name: 'app_api_volunteers')]
+    public function list(VolunteerRepository $volunteerRepository): array
+    {
+        return $volunteerRepository->findAll();
+    }
+}
